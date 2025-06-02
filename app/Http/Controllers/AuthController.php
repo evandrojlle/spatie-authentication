b1c1dd641f8708de1f2bfeb46bab0d0d19e1078c
Evandro de Oliveira <evandro.oliveira@univille.br>
2025-06-02 13:48:36 -0300

    Criada a controller responsável pela autenticação

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = JWTAuth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])) {
+            return response()->json(['error' => 'Credenciais inválidas'], 401);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Logout realizado com sucesso']);
+    }
+
+    public function me()
+    {
+        return response()->json(JWTAuth::user());
+    }
+}
