f3ec70ae5c46ebbff97815a76ce60009ec674e03
Evandro de Oliveira <evandro.oliveira@univille.br>
2025-06-03 10:41:19 -0300

    Criadas as rotas de Autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/* 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+ */
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
+});
